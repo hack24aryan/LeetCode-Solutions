b07187cd884327ec816268284e7d8c1611812660
Tapash Kumar Das <60435929+hack24aryan@users.noreply.github.com>
2026-08-31 01:16:05 +0530

    feat: solve #2091 Removing Minimum and Maximum From Array [C]

---
+++ 2091-removing-minimum-and-maximum-from-array/solution.c
@@ -0,0 +1,25 @@
+            indexmax=i;}
+            indexmin=i;
+            max=nums[i];
+}        if(max<nums[i]){
+
+    }
+    int smallindex,largeindex;
+
+
+    smallindex=indexmin<indexmax?indexmin:indexmax;
+    largeindex=indexmin>indexmax?indexmin:indexmax;
+    int fromfront=largeindex+1;
+
+    int fromback=numsSize-smallindex;
+    int both=(smallindex+1)+(numsSize-largeindex);
+    int answer = fromfront;
+
+            min=nums[i];
+        if(min>nums[i]){
+    {
+    for(i=0;i<numsSize;i++)
+    int i,indexmin=0,indexmax=0;
+
+int minimumDeletions(int* nums, int numsSize) {
+    int min=nums[0],max=nums[0];
